791c7b5b641de798be7b9a0ebca7f1a513d455dc
PerfilovStanislav <stanislav.perfilov@gmail.com>
2026-07-02 23:28:10 +0200

    performance

--- back/src/analyzer/analyzer.go
+++ back/src/analyzer/analyzer.go
@@ -4,8 +4,11 @@ import (
 	"config"
 	"fmt"
 	"indicator"
+	"math"
 	"runtime"
 	"source"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/fatih/color"
@@ -33,10 +36,11 @@ type TpSlClose struct {
 type Coef float64
 
 type Analyzer struct {
-	cfg     config.AnalyzerConfig
-	quotes  Quotes
-	ln      int
-	Results chan TaskResult
+	cfg         config.AnalyzerConfig
+	quotes      Quotes
+	ln          int
+	Results     chan TaskResult
+	maxCoefBits uint64
 }
 
 type IndicatorParams struct {
@@ -76,6 +80,20 @@ func New(cfg config.AnalyzerConfig, quotes Quotes) *Analyzer {
 	}
 }
 
+func (a *Analyzer) updateMaxCoef(val float64) bool {
+	for {
+		currentBits := atomic.LoadUint64(&a.maxCoefBits)
+		currentVal := math.Float64frombits(currentBits)
+		if val <= currentVal {
+			return false
+		}
+		newBits := math.Float64bits(val)
+		if atomic.CompareAndSwapUint64(&a.maxCoefBits, currentBits, newBits) {
+			return true
+		}
+	}
+}
+
 func (a *Analyzer) Run() {
 	runtime.GOMAXPROCS(a.cfg.Threads)
 
@@ -87,29 +105,37 @@ func (a *Analyzer) Run() {
 
 	fmt.Printf("IndicatorsCompares len:%d\n", len(IndicatorsCompares))
 
-	taskChannel := make(chan Task)
-	ready := make(chan struct{}, a.cfg.Threads)
+	type compareJob struct {
+		ic      IndicatorsCompare
+		signals []int
+	}
 
-	for i := 0; i < a.cfg.Threads; i++ {
-		go func(taskChannel <-chan Task, ready chan<- struct{}) {
-			for task := range taskChannel {
-				a.testTask(task)
+	jobs := make([]compareJob, 0, len(IndicatorsCompares))
+	for ic, signals := range IndicatorsCompares {
+		jobs = append(jobs, compareJob{ic, signals})
+	}
+
+	numJobs := int64(len(jobs))
+	var index int64 = 0
+	var wg sync.WaitGroup
+
+	for w := 0; w < a.cfg.Threads; w++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				idx := atomic.AddInt64(&index, 1) - 1
+				if idx >= numJobs {
+					break
+				}
+				job := jobs[idx]
+				for tpSlParam, tpSlClose := range TpSlCloses {
+					a.testTaskDirect(job.ic, job.signals, tpSlParam, tpSlClose.coefs, tpSlClose.indexes)
+				}
 			}
-			ready <- struct{}{}
-		}(taskChannel, ready)
+		}()
 	}
-
-	for indicatorsCompare, _ := range IndicatorsCompares {
-		for tpSlParam, _ := range TpSlCloses {
-			taskChannel <- Task{indicatorsCompare, tpSlParam}
-		}
-	}
-	close(taskChannel)
-
-	for i := 0; i < a.cfg.Threads; i++ {
-		<-ready
-	}
-	close(ready)
+	wg.Wait()
 
 	close(a.Results)
 	<-resultDoneSignal
@@ -129,14 +155,11 @@ func (a *Analyzer) Run() {
 	//fmt.Println(tpCnt, slCnt, len(s.quotes.Closes))
 }
 
-func (a *Analyzer) testTask(t Task) {
+func (a *Analyzer) testTaskDirect(ic IndicatorsCompare, signals []int, tpSlParam TpSlParam, coefs []float64, indexes []int) {
 	// здесь берём один уровень. Если надо будет сравнивать 3 индикатора, то придётся позаморачиваться
 
 	var (
 		result       = 1.0
-		signals      = IndicatorsCompares[t.IndicatorsCompare]
-		coefs        = TpSlCloses[t.TpSlParam].coefs
-		indexes      = TpSlCloses[t.TpSlParam].indexes
 		closingIndex = -1
 	)
 
@@ -150,26 +173,29 @@ func (a *Analyzer) testTask(t Task) {
 	}
 
 	if result > 1.0 {
-		a.Results <- TaskResult{
-			Task: t,
-			Coef: result,
+		currentMax := math.Float64frombits(atomic.LoadUint64(&a.maxCoefBits))
+		if result > currentMax {
+			if a.updateMaxCoef(result) {
+				a.Results <- TaskResult{
+					Task: Task{
+						IndicatorsCompare: ic,
+						TpSlParam:         tpSlParam,
+					},
+					Coef: result,
+				}
+			}
 		}
 	}
 }
 
 func (a *Analyzer) resultChannelHandler() <-chan struct{} {
-	maxCoef := 0.0
-
 	done := make(chan struct{})
 
 	go func() {
 		defer close(done)
 
 		for r := range a.Results {
-			if r.Coef > maxCoef {
-				maxCoef = r.Coef
-				fmt.Println(r)
-			}
+			fmt.Println(r)
 		}
 	}()
 
@@ -218,23 +244,51 @@ func (a *Analyzer) fillTakeprofitStoplossParams() {
 }
 
 func (a *Analyzer) fillIndicatorsCompares() {
+	type compareJob struct {
+		compareParams IndicatorsCompare
+	}
+
+	var jobs []compareJob
 	for i := 0; i < len(IndicatorParamGroups)-1; i += 1 {
 		currentParams := IndicatorParamGroups[i]
 
 		for _, currentParam := range currentParams {
 			for _, nextParam := range IndicatorParamGroups[i+1] {
-				compareParams := IndicatorsCompare{
-					Indicator1Params: currentParam,
-					Indicator2Params: nextParam,
-				}
-				indexes := a.compareIndicators(compareParams)
-
-				if len(indexes) >= a.cfg.MinSignals {
-					IndicatorsCompares[compareParams] = indexes
-				}
+				jobs = append(jobs, compareJob{
+					compareParams: IndicatorsCompare{
+						Indicator1Params: currentParam,
+						Indicator2Params: nextParam,
+					},
+				})
 			}
 		}
 	}
+
+	numJobs := int64(len(jobs))
+	var index int64 = 0
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	for w := 0; w < a.cfg.Threads; w++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				idx := atomic.AddInt64(&index, 1) - 1
+				if idx >= numJobs {
+					break
+				}
+				job := jobs[idx]
+				indexes := a.compareIndicators(job.compareParams)
+				if indexes != nil {
+					mu.Lock()
+					IndicatorsCompares[job.compareParams] = indexes
+					mu.Unlock()
+				}
+			}
+		}()
+	}
+	wg.Wait()
 }
 
 func (a *Analyzer) getPricesBySource(t source.Type) []float64 {
@@ -347,15 +401,26 @@ level2:
 }
 
 func (a *Analyzer) compareIndicators(p IndicatorsCompare) []int {
-	var (
-		currentPrices = IndicatorPrices[p.Indicator1Params]
-		nextPrices    = IndicatorPrices[p.Indicator2Params]
-		indexes       = make([]int, 0)
-	)
+	currentPrices := IndicatorPrices[p.Indicator1Params]
+	nextPrices := IndicatorPrices[p.Indicator2Params]
 
-	for i := 0; i < a.ln-1; i++ { // don't check last candle
+	matchCount := 0
+	for i := 0; i < a.ln-1; i++ {
 		if nextPrices[i] > currentPrices[i] {
-			indexes = append(indexes, i)
+			matchCount++
+		}
+	}
+
+	if matchCount < a.cfg.MinSignals {
+		return nil
+	}
+
+	indexes := make([]int, matchCount)
+	idx := 0
+	for i := 0; i < a.ln-1; i++ {
+		if nextPrices[i] > currentPrices[i] {
+			indexes[idx] = i
+			idx++
 		}
 	}
 
